ca9e61ddf255313db6e634596afd36240821e834
Marma__duk <160778212+Marmaduk0x@users.noreply.github.com>
2024-04-26 13:45:31 +0200

    update error handling

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,15 +1,21 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract EnhancedSimpleStorage {
     
     uint256 private _storedNumber;
 
+    constructor() {
+        _storedNumber = 0;
+    }
+
     function storeNumber(uint256 number) public {
         _storedNumber = number;
+        emit NumberStored(_storedNumber);
     }
 
     function retrieveNumber() public view returns (uint256){
         return _storedNumber;
     }
+
+    event NumberStored(uint256 indexed newNumber);
 }
