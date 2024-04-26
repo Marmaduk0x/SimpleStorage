339c86e8366d07ff00d27548e47eee95aa3a672b
Marma__duk <160778212+Marmaduk0x@users.noreply.github.com>
2024-04-26 18:17:13 +0200

    error catching

--- deploy.js
+++ deploy.js
@@ -2,17 +2,24 @@ const { ethers } = require("hardhat");
 require("dotenv").config();
 
 async function main() {
-  await hre.run("compile");
-  const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
-  console.log("Deploying SimpleStorage...");
-  const simpleStorage = await SimpleStorage.deploy();
-  await simpleStorage.deployed();
-  console.log(`SimpleStorage deployed to: ${simpleStorage.address}`);
+  try {
+    await hre.run("compile");
+
+    const SimpleStorage = await ethers.getContractFactory("SimpleStorage"); 
+    console.log("Deploying SimpleStorage...");
+
+    const simpleStorage = await SimpleStorage.deploy(); 
+    await simpleStorage.deployed();
+    console.log(`SimpleStorage deployed to: ${simpleStorage.address}`);
+  } catch (error) {
+    console.error("An error occurred during the deployment process:", error);
+    process.exit(1);
+  }
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
+    console.error("An unexpected error occurred:", error);
     process.exit(1);
   });
